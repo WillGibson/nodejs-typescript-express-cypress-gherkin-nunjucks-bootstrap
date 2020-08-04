82285db24a48da3b31982ae06bfca4cb39384cdb
Will Gibson <will.gibson@walterego.co.uk>
2020-08-04 17:40:29 +0100

    Try listening on the PORT env var from Heroku

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@ import express from 'express';
 
 const app: express.Application = express();
 
+const listeningPort = process.env.PORT || 3000;
+
 app.get('/', function (req, res) {
     res.send('<html><body><h1>Hello World!</h1></body></html>');
 });
-app.listen(3000, function () {
-    console.log('Listening on http://localhost:3000');
+
+app.listen(listeningPort, function () {
+    console.log(`Listening on http://localhost:${listeningPort}`);
 });
